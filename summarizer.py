9a43d2134a1afe2d07ffe3e67279a3591dfb6d42
bibekjit <77575222+bibekjit@users.noreply.github.com>
2022-04-20 08:57:29 +0530

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -1,134 +1,115 @@
 import pandas as pd
-import numpy as np
 from math import log
-import spacy
 import os
+import spacy
 from numpy.linalg import svd
 pd.options.mode.chained_assignment = None
 
-MODEL_NAME="en_core_web_md"
+MODEL_NAME = "en_core_web_md"
+FILE_PATH = input('Enter path to text file : ')
+SUMMARY_SIZE = input('Enter summary size : ')
+docs = []
 
-FILE_PATH=input("Path to .txt file : ")
-SUMMARY_SIZE=input("Summary size (percentage): ")
 
-def computeSVD(x):
-    """
-    Computes the SVD of the given matrix
-    AV=US
-    
-    :param x: numpy array
-    :return: U,S,V.T
-    """
-    u,s,v=svd(x)
-    e=list(s)
-    s=np.zeros(x.shape)
-    for i in range(len(e)):
-        s[i][i]=e[i]
-    return u,s,v
-
-def summarizer(filePath,summarySize):
-    """
-    Creates the summary of the given text
-
-    :param filePath: path to .txt file
-    :param summarySize: desired summary size (percent of original text)
-                        default size = 40
-
-    :return: creates a .txt file
-    """
+def text_to_tokens(text):
     global MODEL_NAME
-    
-    if summarySize=='':
-        summarySize=0.4
-    else:
-        summarySize=int(summarySize)/100
-    
-    # load the text file
-    with open(filePath,'r',encoding='utf-8') as f:
-        text=f.read().split('\n')
-        text=' '.join(text).strip()
+    model = spacy.load(MODEL_NAME)
+    tokens = model(text)
+    tokens = [t for t in tokens if (not t.is_stop) and (not t.is_punct)]
+    tokens = [str(t.lemma_).lower() for t in tokens if not t.is_space]
+    tokens = [t for t in tokens if not t.isdigit()]
+    return list(set(tokens))
 
-    # load the spacy model
-    model=spacy.load(MODEL_NAME)
 
-    # create tokens 
-    tokens=model(text)
-    tokens=[t for t in tokens if (not t.is_stop) and (not t.is_punct)]
-    tokens=[str(t.lemma_).lower() for t in tokens if not t.is_space]
-    tokens=list(set(tokens))
+def text_to_sentences(text):
+    global MODEL_NAME
+    global docs
+    sentences = []
 
-    sentences=[]
-    doc=[]
-    sent=''
+    model = spacy.load(MODEL_NAME)
+    sent = ''
 
     for w in text.split():
 
-        sent=sent+" "+w
-        if (w[-1]=='.') and (not w[-2].isupper()):
-            doc.append(sent.strip())
-            sent_tokens=model(sent)
-            sent_tokens=[str(t.lemma_).lower() for t in sent_tokens if not t.is_punct]
-            sent=' '.join(sent_tokens).strip()
-            sentences.append(sent[:])
-            sent='' 
+        sent = sent + " " + w
+        if (w[-1] == '.') and (not w[-2].isupper() or w.isupper()):
+            docs.append(sent.strip())
+            sent_tokens = model(sent)
+            sent_tokens = [str(t.lemma_).lower() for t in sent_tokens if not t.is_punct]
+            sent = ' '.join(sent_tokens).strip()
+            sentences.append(sent)
+            sent = ''
+    return sentences
 
-    # create dataframe to create TF-IDF input matrix
-    df=pd.DataFrame(columns=['sent'+str(s) for s in range(len(sentences))],index=tokens)
-    df.fillna(0,inplace=True)
 
-    # TF-IDF
+def create_input_matrix(sentences, tokens):
+    mat = pd.DataFrame(columns=[s for s in range(len(sentences))], index=tokens)
+    mat.fillna(float(0), inplace=True)
+    for t in mat.index:
+        count = 0
+        for i, sent in enumerate(sentences):
+            if t in sent.split():
+                count += 1
 
-    # compute sentence frequency and inverse sentence frequency (sf and isf) for each sentence
-    sf=0
-    tfidf=[]
-    for word in df.index:
-        for i,sent in enumerate(sentences):
-            if word in sent.split():
-                sf+=1
+        idf = log(len(sentences) / (count + 1))
 
-        isf=log(len(sentences)/sf + 1)
+        for i, sent in enumerate(sentences):
+            tf = sent.split().count(t) / len(sent.split())
+            mat[i].loc[t] = tf * idf
 
-        # compute word frequency for each word in each sentence
-        # and get the tfidf score
-        for i,sent in enumerate(sentences):
-            if word in sent.split():
-                wf=sent.split().count(word)/len(sent.split())
-                df['sent'+str(i)].loc[word]=wf*isf 
-                tfidf.append(wf*isf)
+    return mat
 
-    # create input matrix and get the svd
-    A=df.values
-    u,s,v=computeSVD(A)
 
-    # get average score and replace the scores with 0 
-    # if score is less than equal to average
+def summarize_text(input_matrix,summ_size):
+    u, s, v = svd(input_matrix)
+
     for i in range(len(v.T)):
-        row=v.T[i]
-        mean=sum(row)/len(row)
-        for j,val in enumerate(row):
-            if val <= mean: 
-                row[j]=0
+        row = v.T[i]
+        mean = sum(row) / len(row)
+        for j, val in enumerate(row):
+            if val <= mean:
+                row[j] = 0
 
-    # get sentences for summary, with the top score sum
-    sent_scores={}
+    sent_scores = {}
     for i in range(len(v)):
-        sent_scores[sum(v[i])]=i
-    num=int(np.round(len(doc)*summarySize))      
-    top=sorted(list(sent_scores))[-num:]
+        if sum(v[i]) > 0:
+            sent_scores[i] = sum(v[i])
 
-    # print the summary
-    summary=""
-    for score in top:
-        i=sent_scores[score]
-        summary=summary+" "+doc[i]
-    summary=summary.strip()
+    sent_scores = {k: v for k, v in sorted(sent_scores.items(), key=lambda item: item[1], reverse=True)}
+    sents = list(sent_scores)
+    k = round(len(sents)*summ_size)
+    summ_sents = sorted(sents[:k])
 
-    filename=os.path.basename(filePath).split('.')[0]
-    output_file_name=filename+'_summarized_'+str(int(summarySize*100))+".txt"
-    
-    with open(output_file_name,"w") as f:
+    summary = ""
+
+    for i in summ_sents:
+        summary = summary + " " + docs[i]
+
+    return summary.strip()
+
+
+def main(file_path, summary_size):
+
+    with open(file_path,'r') as file:
+        text = file.read().split('\n')
+        text = ' '.join(text).strip()
+
+    if summary_size == "":
+        summary_size = 0.6
+    else: summary_size = int(summary_size)/100
+    tokens = text_to_tokens(text)
+    sentences = text_to_sentences(text)
+    input_matrix = create_input_matrix(sentences,tokens)
+    summary = summarize_text(input_matrix,summary_size)
+
+    filename = os.path.basename(file_path).split('.')[0]
+    output_file_name = filename + '_summarized_' + str(int(summary_size * 100)) + ".txt"
+
+    with open(output_file_name, "w") as f:
         f.write(summary)
         
-    print("\nSummary created !!")
+    print('summary created !')
 
-summarizer(FILE_PATH,SUMMARY_SIZE)
+
+main(file_path=FILE_PATH,summary_size=SUMMARY_SIZE)
+
